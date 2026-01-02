815f38d11c4ed9d8d0420462f56c1458de762f72
codezcracker <codez.cracker@gmail.com>
2026-01-02 05:15:15 +0500

    Fix automation-rules exports and update Next.js

---
+++ src/lib/automation-rules.js
@@ -0,0 +1,17 @@
+// Automation rules helper functions
+
+export async function checkAutomationRules(deviceId, sensorData) {
+  // TODO: Implement automation rule checking
+  return null;
+}
+
+export async function getAutomationRules(userId) {
+  // TODO: Implement get automation rules
+  return [];
+}
+
+export async function updateAutomationRules(userId, rules) {
+  // TODO: Implement update automation rules
+  return { success: true };
+}
+
